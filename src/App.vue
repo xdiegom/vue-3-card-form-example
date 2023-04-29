c7240bb807923a84baccab48a45a15b4dec69d57
Diego Melgar <diego.melgar.sv@gmail.com>
2023-04-29 16:38:12 -0600

    📦 add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Hello world</div>
+  <div class="w-full min-h-screen items-center justify-center flex">
+    <div class="bg-gradient-to-r from-cyan-500 to-blue-500 text-center border-orange-500 border-2">
+      <h1 class="text-xl font-bold h-14 p-3 w-[400px]">
+        Vue Payment Information Form
+      </h1>
+    </div>
+  </div>
 </template>
 
 <script setup>
