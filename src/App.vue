5469afec428b5839713baf303ffc6f56d3615be4
Diego Melgar <diego.melgar.sv@gmail.com>
2023-04-29 18:21:07 -0600

    🎨 create card component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,78 @@
 <template>
   <div class="w-full min-h-screen items-center justify-center flex">
-    <div class="bg-gradient-to-r from-cyan-500 to-blue-500 text-center border-orange-500 border-2">
-      <h1 class="text-xl font-bold h-14 p-3 w-[400px]">
-        Vue Payment Information Form
-      </h1>
-    </div>
+    <Card :information="cardCategory"></Card>
   </div>
 </template>
 
-<script setup>
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      currentCategory: 0,
+      cards: [
+        {
+          name: "Basic",
+          background: "/images/card-bgs/basic.png",
+          cardHolder: {
+            name: 'John Doe'
+          },
+          card: {
+            number: '1234 5457 9832 0329',
+            expires: '03/24'
+          }
+        },
+        {
+          name: "Gold",
+          background: "/images/card-bgs/gold.png",
+          cardHolder: {
+            name: 'Jane Doe'
+          },
+          card: {
+            number: '1234 5457 9832 0415',
+            expires: '04/25'
+          }
+        },
+        {
+          name: "Platinum",
+          background: "/images/card-bgs/platinum.png",
+          cardHolder: {
+            name: 'Marie Doe'
+          },
+          card: {
+            number: '1234 5457 9832 0712',
+            expires: '12/24'
+          }
+        },
+        {
+          name: "Black",
+          background: "/images/card-bgs/black.png",
+          cardHolder: {
+            name: 'Anthony Doe'
+          },
+          card: {
+            number: '1234 5457 9832 2849',
+            expires: '09/24'
+          }
+        },
+      ],
+      cardCategory: null,
+    };
+  },
+  created() {
+    this.cardCategory = this.cards[this.currentCategory];
+
+    setInterval(() => {
+      this.currentCategory =
+        this.currentCategory + 1 > this.cards.length ? 0 : this.currentCategory;
+
+      this.cardCategory = this.cards[this.currentCategory];
+      this.currentCategory += 1;
+    }, 1000);
+  },
+}
 </script>
